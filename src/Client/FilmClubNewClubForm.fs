e285040045cb79a60837392a26658b8208b760b4
Frederick Swadling <frederick.swadling@l-p-a.com>
2020-04-12 20:33:42 +0200

    began adding create club form

--- src/Client/FilmClubNewClubForm.fs
+++ src/Client/FilmClubNewClubForm.fs
@@ -28,7 +28,26 @@ let private stream model msgs =
     |> AsyncRx.tag "msgs"
 
 let private view (model : Model) (dispatch : Msg -> unit) =
-    div [] [ str "Hello world"]
+    Container.container [ Container.IsFluid; Container.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Left) ] ] [
+        Content.content [ ] [
+            h1 [] [str "Create new club" ]
+            form [] [
+                Field.div [ ] [
+                    Label.label [ ] [ str "Name"]
+                    Control.div [ ] [ Input.text [ Input.Placeholder "Example club name" ]]]
+                Field.div [ ] [
+                    File.file [ File.HasName ] [ File.label [ ] [
+                        File.input [ ]
+                        File.cta [ ] [
+                            File.icon [ ] [
+                                Icon.icon [ ] [
+                                    Fable.FontAwesome.Fa.i [ Fable.FontAwesome.Free.Fa.Solid.Upload ]  [ ] ] ]
+                            File.label [ ] [
+                                str "Choose an image for the club..." ] ]
+                        File.name [ ] [
+                            str "Club image" ] ] ] ]
+            ]
+        ] ]
 
 let Component (api: IFilmClubApi) (user: IAuth0UserProfile) =
     let model = init
