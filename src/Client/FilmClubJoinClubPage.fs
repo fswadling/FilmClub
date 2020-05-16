1d49b5dff6e1b277e6d383c0b390e514f4570717
Frederick Swadling <frederick.swadling@l-p-a.com>
2020-05-17 00:24:44 +0200

    Finished join club page

--- src/Client/FilmClubJoinClubPage.fs
+++ src/Client/FilmClubJoinClubPage.fs
@@ -15,7 +15,7 @@ open Routes
 
 
 type private Model = {
-    Test: string
+    Requests: ClubJoinRequest list option
 }
 
 type private JoinClubArgs = {
@@ -26,36 +26,65 @@ type private JoinClubArgs = {
 type private MyMsg =
     | RequestJoinClub of JoinClubArgs
     | RequestMade of Response<ClubJoinRequest>
+    | LoadPendingRequests of ClubJoinRequest list
 
 let private init =
-    { Test = "test" }
+    { Requests = None }
 
 let private update (dispatchRoute: Route -> unit) (model : Model) (msg : MyMsg) : Model =
     match msg with
-    | RequestMade _ ->
-        model
+    | RequestMade requestResponse ->
+        match requestResponse with
+        | Valid request ->
+            { model with Requests = Some (request::(model.Requests |> Option.defaultValue [])) }
+        | _ ->
+            model
     | RequestJoinClub args ->
         model
+    | LoadPendingRequests requests ->
+        { model with Requests = Some requests }
 
 let private makeCall (api: IFilmClubApi) (args: JoinClubArgs) =
     AsyncRx.ofAsync (api.RequestJoinClub args.UserId args.ClubId)
 
-let private stream (api: IFilmClubApi) (model: Model) (msgs: IAsyncObservable<MyMsg>) =
-    let joinClubRequests =
-        msgs
-        |> AsyncRx.choose (function | RequestJoinClub joinArgs -> Some joinArgs | _ -> None)
-        |> AsyncRx.flatMapLatest (fun args -> makeCall api args)
-        |> AsyncRx.map RequestMade
+let private stream (api: IFilmClubApi) (user: IAuth0UserProfile) (model: Model) (msgs: IAsyncObservable<MyMsg>) =
+    match model.Requests with
+    | None ->
+        (api.GetJoinClubRequestsForUser user.sub)
+        |> AsyncRx.ofAsync
+        |> AsyncRx.map LoadPendingRequests
+        |> AsyncRx.tag "loadRequests"
+    | _ ->
+        let joinClubRequests =
+            msgs
+            |> AsyncRx.choose (function | RequestJoinClub joinArgs -> Some joinArgs | _ -> None)
+            |> AsyncRx.flatMapLatest (fun args -> makeCall api args)
+            |> AsyncRx.map RequestMade
 
-    joinClubRequests
-        |> AsyncRx.merge msgs
-        |> AsyncRx.tag "msgs"
+        joinClubRequests
+            |> AsyncRx.merge msgs
+            |> AsyncRx.tag "msgs"
+
+let private getRequestStatusText (requestStatus: ClubJoinRequestStatus) =
+    match requestStatus with
+    | Pending -> Text.div [ Modifiers [ Modifier.TextColor IsGrey ] ] [ str "Pending" ]
+    | Accepted -> Text.div [ Modifiers [ Modifier.TextColor IsSuccess ] ] [ str "Accepted" ]
+    | Denied -> Text.div [ Modifiers [ Modifier.TextColor IsDanger ] ] [ str "Denied" ]
+
+let private createRequestRow (request: ClubJoinRequest) =
+    Content.content [ Content.Props [ Props.ClassName "request" ] ] [
+        p [] [ str ("Request to join club with id: " + request.ClubId.ToString())]
+        getRequestStatusText request.RequestStatus ]
 
 let private view (user: IAuth0UserProfile) (api: IFilmClubApi) (model : Model) (dispatch : MyMsg -> unit)  =
     div [] [
         FilmClubJoinClubForm.Component (fun id -> dispatch (RequestJoinClub { UserId = user.sub; ClubId = id})) ()
-        FilmClubPendingJoinClubRequests.Component api user () ]
+        Container.container [ Container.IsFluid; Container.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Left) ] ] [
+        yield! match model.Requests with
+                | Some requests ->
+                    List.map createRequestRow requests
+                | None -> [ Icon.icon [Icon.Size IsLarge ] [ Fable.FontAwesome.Fa.i [ Fable.FontAwesome.Free.Fa.Solid.Spinner; Fable.FontAwesome.Fa.Spin ] []] ] ] ]
 
 let Component (api: IFilmClubApi) (dispatchRoute: Route -> unit) (user: IAuth0UserProfile) =
     let model = init
-    Reaction.StreamComponent model (view user api) (update dispatchRoute) (stream api)
+    Reaction.StreamComponent model (view user api) (update dispatchRoute) (stream api user)
